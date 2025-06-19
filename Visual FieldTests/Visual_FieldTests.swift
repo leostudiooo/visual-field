cf06605d893d8c92311c1a41b2f0a36ecb106572
leostudiooo <lilingfeng0408@outlook.com>
2025-06-19 17:12:51 +0800

    Initial Commit

---
+++ Visual FieldTests/Visual_FieldTests.swift
@@ -0,0 +1,17 @@
+//
+//  Visual_FieldTests.swift
+//  Visual FieldTests
+//
+//  Created by Leo Li on 2025/6/19.
+//
+
+import Testing
+@testable import Visual_Field
+
+struct Visual_FieldTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Visual FieldUITests/Visual_FieldUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Visual_FieldUITestsLaunchTests.swift
+//  Visual FieldUITests
+//
+//  Created by Leo Li on 2025/6/19.
+//
+
+import XCTest
+
+final class Visual_FieldUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
